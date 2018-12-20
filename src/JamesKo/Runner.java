1e3d6226acb3ebdf5dd53932411f6bb8c727b07b
BT_1N3_31 <@>
2018-12-20 10:41:27 -0500

    Kevin

---
+++ src/JamesKo/Runner.java
@@ -0,0 +1,20 @@
+package JamesKo;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        int[][] coolArray = {
+                {15,5,9,10},
+                {12,16,11,6},
+                {14,8,13,7}
+        };
+        Position[][] successors = Successors.getSuccessorArray(coolArray);
+
+        for(int i = 0; i < successors.length; i++){
+            for(int j = 0; j < successors[i].length;j++){
+                System.out.print(successors[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+}
